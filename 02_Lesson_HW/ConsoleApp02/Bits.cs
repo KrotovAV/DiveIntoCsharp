5aa3a6a0746b13083f7365f7e764647f9df30b2b
KrotovAV <KrotovAV@tut.by>
2023-10-29 21:30:31 +0300

    02_Lesson_02_done

---
+++ 02_Lesson_HW/ConsoleApp02/Bits.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp02
+{
+    public class Bits
+    {
+        public Bits(byte b)
+        {
+            this.Value = b;
+        }
+        public byte Value { get; private set; }
+
+        public bool this[int index]
+        {
+            get
+            {
+                if (index > 7 || index < 0)
+                    return false;
+                return ((Value >> index) & 1) == 1;
+            }
+            set
+            {
+                if (index > 7 || index < 0) return;
+                if (value == true)
+                    Value = (byte)(Value | (1 << index));
+                else
+                {
+                    var mask = (byte)(1 << index);
+                    mask = (byte)(0xff ^ mask);
+                    Value &= (byte)(Value & mask);
+                }
+            }
+        }
+        public static implicit operator byte(Bits b) => b.Value; //явное преобразования
+        public static explicit operator Bits(byte b) => new Bits(b); //неявное преобразование
+    }
+}
---
+++ 02_Lesson_HW/ConsoleApp02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Габиль, рашение благополучно нашёл в нашей методичке на 29 странице.
+            //Ну и переписал сюда
+            var a = new Bits(0);
+            for (int i =0; i <= 7; i++)
+            {
+                if (i%2 == 0)
+                    a[i] = true;               
+            }
+            Console.WriteLine(a.Value.ToString());
+            for (int i = 7; i >= 0; i--)
+            {
+                Console.WriteLine(a[i] + " ");
+            }
+
+            var bits = new Bits(10);
+            Console.WriteLine("Type of bits - " + bits.GetType());
+            byte b = bits;
+            Console.WriteLine("Type of b - " + b.GetType());
+
+            b = 20;
+            Console.WriteLine("Type of b - " + b.GetType());
+            bits = (Bits)b;
+            Console.WriteLine("Type of bits - " + bits.GetType());
+        }
+    }
+}
